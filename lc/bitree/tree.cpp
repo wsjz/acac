5448879d631af454b19e6e62185384dad9d0aaa5
slothever <jinzhe@selectdb.com>
2023-05-03 00:18:03 +0800

    add 124 one side max

--- lc/bitree/tree.cpp
+++ lc/bitree/tree.cpp
@@ -3,10 +3,28 @@
 
 using namespace std;
 
+/**
+ * lc 124
+ */
 class Solution {
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+};
+
 public:
-    static vector<int> tree(vector<int>& nums, int target) {
+    int oneSideMax(TreeNode* root) {
+        if (root == nullptr) {
+            return 0;
+        }
+        int left = max(0, oneSideMax(root->left));
+        int right = max(0, oneSideMax(root->right));
+        ans = max(ans, left + right + root->val);
+        return max(left, right) + root->val;
     }
+private:
+    int ans = INT32_MIN;
 };
 
 int main() {
--- lc/test.cpp
+++ lc/test.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+/**
+ * lc 1
+ */
+class Solution {
+public:
+    static vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> map;
+        for (int i = 0; i < nums.size(); i++) {
+            auto iter = map.find(target - nums[i]);
+            if (iter != map.end()) {
+                return {iter->second, i};
+            }
+            map.insert(pair<int, int>(nums[i], i));
+        }
+        return {};
+    }
+};
 
 int main() {
-    
-    std::cout << "c++ version = " << __cplusplus << std::endl;
+    Solution solution;
+    vector<int> arr = {3, 2, 4};
+    auto res = solution.twoSum(arr, 6);
+    for_each(res.begin(), res.end(), [](const auto &i) { cout << i << " "; });
 }
